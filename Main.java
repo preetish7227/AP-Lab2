0698e2703328b6b6bf3a9987147092bac2b46fd3
preetish7227 <241b192@juetguna.in>
2026-08-14 14:51:47 +0530

    Day 1

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP
 C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
 Code, Compile, Run and Debug online from anywhere in world.
 
-*******************************************************************************/
+******************************************************************************/
 
 public class Main
 {
